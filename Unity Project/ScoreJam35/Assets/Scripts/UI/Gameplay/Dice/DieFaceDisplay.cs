d3bad8bd2784bc39d707b7237c1eb345f3dee872
BarBenzvi <bar_bar@live.com>
2025-09-27 13:21:02 -0700

    dice that look like dice

--- Unity Project/ScoreJam35/Assets/Scripts/UI/Gameplay/Dice/DieFaceDisplay.cs
+++ Unity Project/ScoreJam35/Assets/Scripts/UI/Gameplay/Dice/DieFaceDisplay.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class DieFaceDisplay : MonoBehaviour
 {
-    TextMeshProUGUI text = null;
+    public List<Sprite> FaceSprites = new List<Sprite>();
+
+    public Image TargetImage = null;
     DieLogic dieLogic = null;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        text = GetComponentInChildren<TextMeshProUGUI>();
         dieLogic = GetComponent<DieLogic>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = dieLogic.CurrentValue.ToString();
+        TargetImage.sprite = FaceSprites[dieLogic.CurrentValue - 1];
     }
 }
