493393def6481da3fc6e51ceb9bbd65f765f2c31
Haduken4 <bar_bar@live.com>
2025-09-29 08:49:13 -0700

    day counter

---
+++ Unity Project/ScoreJam35/Assets/Scripts/UI/Display/Score/DayDisplayText.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayDisplayText : MonoBehaviour
+{
+    public Image TargetImage = null;
+    public TextMeshProUGUI TextTarget = null;
+
+    public Sprite DayIcon = null;
+    public Sprite NightIcon = null;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TurnManager.Instance.OnStartDay += StartDay;
+        TurnManager.Instance.OnStartNight += StartNight;
+    }
+
+    void StartDay()
+    {
+        TargetImage.sprite = DayIcon;
+        TextTarget.text = TurnManager.Instance.GetCurrentDay().ToString();
+    }
+
+    void StartNight()
+    {
+        TargetImage.sprite = NightIcon;
+        TextTarget.text = TurnManager.Instance.GetCurrentDay().ToString();
+    }
+}
